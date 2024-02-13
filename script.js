67bb528f2de63c4f2d26925ad6a21621a207d740
neelpatkar007 <149313087+neelpatkar007@users.noreply.github.com>
2024-02-13 18:05:05 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function addHoverEffectveg(element) {
+  element.style.backgroundColor = "#45a049";
+  element.style.color = "#fff";
+}
+
+function removeHoverEffectveg(element) {
+  element.style.backgroundColor = "#4CAF50";
+  element.style.color = "#fff";
+}
+function addHoverEffectnonveg(element) {
+  element.style.backgroundColor = "#dd6464";
+  element.style.color = "#fff";
+}
+
+function removeHoverEffectnonveg(element) {
+  element.style.backgroundColor = "#e15b69";
+  element.style.color = "#fff";
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  // Add form submission logic here (e.g., form validation, sending data to a server)
+  alert('Form submitted successfully!');
+}
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
+function myFunction() {
+  var popup = document.getElementById("myPopup");
+  popup.classList.toggle("show");
+}
